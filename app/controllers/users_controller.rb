8e02a875f5225c1a3cd9210cd08a8306ec5988b2
tshirt519 <tshirt519@gmail.com>
2021-04-16 22:17:30 +0900

    PG rails 11まで　posts controller周りでバグあり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,8 +58,10 @@ class UsersController < ApplicationController
   end
   
   def login
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
+    # メールアドレスのみを用いて、ユーザーを取得するように書き換えてください
+    @user = User.find_by(email: params[:email])
+    # if文の条件を&&とauthenticateメソッドを用いて書き換えてください
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
@@ -78,12 +80,8 @@ class UsersController < ApplicationController
   end
   
   def likes
-    # 変数@userを定義してください
     @user = User.find_by(id: params[:id])
-    
-    # 変数@likesを定義してください
     @likes = Like.where(user_id: @user.id)
-    
   end
   
   def ensure_correct_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_125315) do
+ActiveRecord::Schema.define(version: 2021_04_16_131135) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_125315) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
