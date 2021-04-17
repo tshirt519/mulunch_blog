32dd80837e087ffd84acae1ba25a9e2f9522746b
tshirt519 <tshirt519@gmail.com>
2021-04-17 22:22:12 +0900

    rails 11まで完了、9から再確認してリファクタリング、バックアップしてからherokuデプロイ

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
   validates :user_id, {presence: true}
   validates :post_id, {presence: true}
+
+  def user
+    return User.find_by(id: self.user_id)
+  end
+    
+  def posts
+    return Post.where(user_id: self.id)
+  end
+  
 end
