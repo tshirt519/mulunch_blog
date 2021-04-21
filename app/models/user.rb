34d0703b7e6f352e64657947cd4008ce85a1aec9
tshirt519 <tshirt519@gmail.com>
2021-04-21 14:10:49 +0900

    開発のためvalidation一旦外す

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
+  # has_secure_password
   
-  validates :name, {presence: true}
-  validates :email, {presence: true, uniqueness: true}
+  # validates :name, {presence: true}
+  # validates :email, {presence: true, uniqueness: true}
   
   
   def posts
