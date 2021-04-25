a057a1d4bfdc68e943606158ecc8db34292161a9
tshirt519 <tshirt519@gmail.com>
2021-04-25 14:51:41 +0900

    aws.rb削除

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-  region: 'ap-northeast-1',
-  credentials: Aws::Credentials.new('AKIATPYYG5GPGSDH42XC', '/1mR9fK0x5P4J57VMpcATBu8HqdrKbT/umWzZTey'),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket('mulunch-images')
