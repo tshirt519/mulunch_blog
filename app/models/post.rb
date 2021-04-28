a25eab568bc2d643e874c89cf9200c233efc4af8
tshirt519 <tshirt519@gmail.com>
2021-04-28 22:07:18 +0900

    validation presenceなどは復活

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  # validates :content, {presence: true, length: {maximum: 140}}
-  # validates :user_id, {presence: true}
+  validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id, {presence: true}
   
   def user
     return User.find_by(id: self.user_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  # has_secure_password
-  
-  # validates :name, {presence: true}
-  # validates :email, {presence: true, uniqueness: true}
+  has_secure_password
+  validates :name, {presence: true}
+  validates :email, {presence: true, uniqueness: true}
   
   
   def post
