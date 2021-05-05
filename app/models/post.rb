2980275b41a983aa4ca3d997b1a8799c6099f24d
tshirt519 <tshirt519@gmail.com>
2021-05-05 13:36:54 +0900

    postsのvalidation　タイトルと本文にも

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, {presence: true, length: {maximum: 70}}
   validates :content, {presence: true, length: {maximum: 140}}
+  validates :article1, {presence: true}
   validates :user_id, {presence: true}
   
   def user
