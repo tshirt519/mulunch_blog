e8294fcea0c5313409449ab763f2ac20f2f4d3ab
tshirt519 <tshirt519@gmail.com>
2021-05-08 19:52:36 +0900

    db:migrate実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_061851) do
+ActiveRecord::Schema.define(version: 2021_05_08_104326) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
