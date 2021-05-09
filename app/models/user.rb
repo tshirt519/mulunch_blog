b1eb8824068ac5fa6f21fcc68174d5f4c1db399f
tshirt519 <tshirt519@gmail.com>
2021-05-09 22:41:37 +0900

    usersの画像保存見るためpassのvalidates一旦解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, {presence: true, uniqueness: true}
   validates :email, {presence: true, uniqueness: true}
-  validates :password, {presence: true}
-  
   
   def post
     return Post.where(user_id: self.id)
